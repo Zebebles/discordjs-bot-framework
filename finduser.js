81fede7b7b9e983dcdac69b9f10b2c96a3826157
greennoizuf <zebm54@bigpond.com>
2017-08-19 02:52:48 +1000

    added findUser function

--- finduser.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(){
-    this.findUser = function(message, args){
-          return message.client.users.get(args)
-        || message.client.users.find(userino => userino.username.toLowerCase().includes(args.toLowerCase()))
-        || message.mentions.users.first();
-    }
-}
