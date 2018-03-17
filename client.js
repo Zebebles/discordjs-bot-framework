0eac4ec6852a9e6807f6572c371e63e019873df6
greennoizuf <zebm54@bigpond.com>
2018-03-17 17:47:13 +1100

    way more efficient at checking messages, and has a perms check

--- client.js
+++ client.js
@@ -4,14 +4,11 @@ const fs = require("fs");
 const path = require('path');
    
 class DBFClient extends Client{
-    constructor(options = {}) {
+    constructor(options) {
         super(options);
 
         if(!options.prefix) throw Error("You must specify a prefiix");
         else this.prefix = options.prefix;
-        
-        if(!options.name) throw Error("You must specify a name for your bot");
-        else this.name = options.name;
 
         if(!options.author) throw Error("You must specify an author"); 
         else this.author = options.author;
@@ -22,84 +19,73 @@ class DBFClient extends Client{
         if(!options.cmddir) throw Error("You must specify a command directory"); 
         else this.commandsdir = options.cmddir;
 
-        this._MentionsTrigger = options.MentionsTrigger;
+        this.mentionsTrigger = options.MentionsTrigger;
 
         this.commands = new Array();
 
         this.on("message", msg => {
-            let prefix = msg.client.Prefix;
-            if(msg.guild && msg.guild.prefix) prefix = msg.guild.prefix;
-            let user;
-            let args = "";
-            let command;
-            if(msg.guild && !msg.channel.permissionsFor(msg.guild.me).has("SEND_MESSAGES"))
+            let prefix = (msg.guild && msg.guild.prefix) ? msg.guild.prefix : this.prefix;
+            let user, args, command;
+            if(msg.guild && !msg.channel.permissionsFor(msg.guild.me).has("SEND_MESSAGES")) //If the bot doesn't have perms to talk in the channel.
                 return;
-            if(msg.content.substring(0,2) == "<@" && (msg.content.indexOf(msg.client.user.id) == 3 || msg.content.indexOf(msg.client.user.id) == 2)){//@bot command (maybe @bot)
-                let regex = new RegExp(/[ ]/g);
-                command = msg.content;
-                if(!regex.test(command)) return;
-                command = msg.content.split(" ")[1];
-            }else if(msg.content.substring(0,prefix.length) == prefix || msg.channel.type == "dm"){
-                command = msg.content.trim() + " ";
-                command = command.split(" ")[0].replace(prefix, "").trim();
-            }else return;
-            msg.client.Commands.forEach(cmd => {
-                if(cmd.areYou(command.toLowerCase())){
-                    //spam check
-                    if(msg.author.nextUse && msg.author.nextUse > Date.now())
-                        return msg.reply("Hold up! You're on command cooldown for another **" + ((msg.author.nextUse - Date.now())/1000).toFixed(1) + "** seconds.").then(m => m.delete(2500));
-                    else
-                        msg.author.nextUse = Date.now() + 1000;
-                    //end spam check
-                    if(cmd.OwnerOnly && (msg.author.id != msg.client.Author)) return;
-                    if(cmd.GuildOnly && msg.channel.type != "text") return msg.channel.send("The command **" + cmd.triggers[0] + "** can't be used in private chats.");
-                    if( msg.guild && ((msg.channel.disabledCommands && msg.channel.disabledCommands.find(command => cmd.name == command)) 
-                        || (msg.guild.disabledCommands && msg.guild.disabledCommands.find(command => command == cmd.name))))
-                            return
-                    if(cmd.ReqUser) user = this.findUser(msg);
-                    if(cmd.ReqArgs) args = this.getArgs(msg);
-                    try{
-                        cmd.run({"msg": msg, "user": user, "args": args});
-                    }catch(e){
-                        if(msg.guild)
-                            console.log("Error executing command '" + cmd.name + "' in '" + msg.guild.name + "'\n" + e)
-                        else
-                            console.log("Error executing command '" + cmd.name + "' in dm'\n" + e)
-                    }
-                }
-            });
+
+            var prefixRegex = (this.mentionsTrigger) ? new RegExp(prefix.replace(/[!@#$%^&*()+=\-[\]\\';,./{}|":<>?~_]/g,"\\$&") + '|<@(!)?' + this.user.id + '>', 'g') : new RegExp(prefix.replace(/[!@#$%^&*()+=\-[\]\\';,./{}|":<>?~_]/g,"\\$&"), 'g');
+            
+            command = (msg.content.match(prefixRegex) && msg.content.trim().indexOf(msg.content.match(prefixRegex)[0]) == 0) 
+            /*if msg starts with prefix*/   ? ((msg.content.replace(prefixRegex,"").trim().split(" "))
+                    /*if the command has arguments*/    ? msg.content.replace(prefixRegex,"").trim().split(" ")[0]
+                    /*if its one word*/                 : msg.content.replace(prefixRegex,"").trim())
+            /*if it doesnt start w/ prfx*/  : null;
+            if(!command) //if couldn't find a word after the prefix.
+                return;
+            
+            if(msg.author.nextUse && msg.author.nextUse > Date.now()) //check if the user's spamming commands.
+                return msg.reply("Hold up! You're on command cooldown for another **" + ((msg.author.nextUse - Date.now())/1000).toFixed(1) + "** seconds.").then(m => m.delete(2500));
+            else
+                msg.author.nextUse = Date.now() + 1000; //user can't use another cmd for 1s
+            
+            let cmd = this.commands.find(c => c.areYou(command));//try find a command that matches the command name
+            if(!cmd) //if the command doesn't exist.
+                return;
+            
+            if(cmd.reqPerms.length != 0 && msg.guild){
+                let userMissing = msg.member.missingPermissions(cmd.reqPerms);
+                if(userMissing && userMissing.length != 0)
+                    return msg.channel.send("You need permission `" + userMissing[0] + "` to do that.");
+                let botMissing = msg.guild.me.missingPermissions(cmd.reqPerms);
+                if(botMissing && botMissing.length != 0)
+                    return msg.channel.send("I need permission `" + botMissing[0] + "` to be able to do that.");
+            }
+
+            if(cmd.OwnerOnly && (msg.author.id != msg.client.Author)) return; //if the cmd is owner only and the user isnt the owner
+            
+            if(cmd.GuildOnly && msg.channel.type != "text")  //if the commands guild only and the channel isn't a text ch
+                return msg.channel.send("The command `" + cmd.name + "` can't be used in private chats.");
+                
+            if( msg.guild && ((msg.channel.disabledCommands && msg.channel.disabledCommands.find(command => cmd.name == command)) 
+                || (msg.guild.disabledCommands && msg.guild.disabledCommands.find(command => command == cmd.name))))
+                    return
+                
+            if(cmd.reqUser) //set the user variable if the command needs the user.
+                user = this.findUser(msg);
+                
+            if(cmd.reqArgs) //set the args variable if the command needs the args.
+                args = this.getArgs(msg);
+
+            cmd.run({"msg": msg, "user": user, "args": args}); //run the command 
         });
 
         this.on("ready", () =>{
-            console.log("logged in as " + this.name);
+            console.log("logged in as " + this.user.username);
         });
     }
 
-    get Prefix() { return this.prefix; }
-    set Prefix(value) { this.prefix = value; }
-
-    get MentionsTrigger(){return this._MentionsTrigger;}
-
-    get Name() { return this.name; }
-    set Name(value) { this.name = value; }
-
-    get Author() { return this.author; }
-
-    get Token() { return this.token; }
-    
-    get CommandsDir() { return this.commandsdir; }
-    set CommandsDir(value) { this.commandsdir = value; }
-
-    get Commands(){ return this.commands }
-
-
     login(){
-        
-        fs.readdir(this.CommandsDir, (err, files) => {
+        fs.readdir(this.commandsdir, (err, files) => {
             if(err) return console.log(err);
             files.forEach(file => {
                 if (path.extname(file) == ".js"){
-                    const Command = require(path.join(this.CommandsDir, file));
+                    const Command = require(path.join(this.commandsdir, file));
                     const cmd = new Command();
                     this.commands.push(cmd);
                 }
@@ -112,10 +98,10 @@ class DBFClient extends Client{
     getHelp(message){
         let helpEmbeds = new Array();
         let groups = [];
-        let cmds = this.Commands;
+        let cmds = this.commands;
         cmds.forEach(cmd => {
-            let group = groups.filter(group => group == cmd.Group);
-            if(group.length == 0) groups.push(cmd.Group);
+            let group = groups.filter(group => group == cmd.group);
+            if(group.length == 0) groups.push(cmd.group);
         });
         let helpMsg;
         groups.forEach(group => {
@@ -123,11 +109,11 @@ class DBFClient extends Client{
                 helpEmbeds[helpEmbeds.length - 1].setColor([127, 161, 216]);                            
                 helpMsg = "\n";
                 let groupCommands = cmds.filter(cmd => {
-                    if(group == cmd.Group) return cmd;
+                    if(group == cmd.group) return cmd;
                 });
                 groupCommands.forEach(command =>{
-                     if(command.OwnerOnly && (message.author.id != message.client.Author));
-                     else helpMsg += "\n\n**" + command.Triggers[0] + "** : *" + command.Description + "*"; 
+                     if(command.ownerOnly && (message.author.id != message.client.author));
+                     else helpMsg += "\n\n**" + command.triggers[0] + "** : *" + command.description + "*"; 
                 });
             helpEmbeds[helpEmbeds.length-1].setTitle(group);
             helpEmbeds[helpEmbeds.length-1].setDescription(helpMsg);
--- command.js
+++ command.js
@@ -1,50 +1,26 @@
 class Command{
-    constructor(obj = {
-        name: name,
-        triggers: triggers,
-        description: description,
-        example: example,
-        group: group,
-        guildOnly: guildOnly,
-        ownerOnly: ownerOnly,
-        reqUser: reqUser,
-        reqArgs: reqArgs
-    }) {
+    constructor(obj)
+    {
+        if (!obj.name)
+            throw Error("Must define a command name.");
+        if(!obj.description)
+            throw Error("Must define command description");
+        if (!obj.example)
+            throw Error("Must define a commmand example");
+        if (!obj.group)
+            throw Error("Must define a command group");
         this.name = obj.name;
         this.ownerOnly = obj.ownerOnly;
-        if (obj.group)  this._group = obj.group;
-        if (!obj.triggers) throw Error("Must define a trigger");
-        else  this.triggers = obj.triggers;
+        this.group = obj.group;
+        this.triggers = obj.triggers;
         this.description = obj.description;
-        if(obj.example)
-            this.example = obj.example;
+        this.example = obj.example;
         this.reqUser = obj.reqUser;
         this.group = obj.group;
         this.reqArgs = obj.reqArgs;
-        if (obj.guildOnly)  this.guildOnly = obj.guildOnly;
+        this.guildOnly = obj.guildOnly;
+        this.reqPerms = obj.reqPerms ? obj.reqPerms : new Array(); 
     }
-
-    get Name() { return this.name;  }
-    set Name(value) { this.name = value; }
-
-    get Triggers() { return this.triggers; }
-    set Triggers(value) { this.triggers = value; }
-
-    get OwnerOnly(){return this.ownerOnly;}
-    set OwnerOnly(value){this.ownerOnly = value;}
-
-    get ReqUser(){ return this.reqUser;}
-    
-    get ReqArgs(){return this.reqArgs;}
-
-    get GuildOnly() { return this.guildOnly; }
-    set GuildOnly(value) {this.guildOnly = value; }
-
-    get Group() { return this.group; }
-    set Group(value) { return this.group; }
-
-    get Description() { return this.description; }
-    set Description(value) { this.description = value; }
 
     run() {
         console.log("default command");
