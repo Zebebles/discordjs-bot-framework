c772b7e17df13b765299ad500e0a9284038dfa6a
DeltaRade <46230290+DeltaRade@users.noreply.github.com>
2019-01-13 21:44:59 -0400

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,21 @@
 declare module 'discordjs-bot-framework' {
+  import Discord from 'discord.js'
 export class Client{
-constructor()
+constructor(options)
+  
+  public login():void
+  public loadCommands():void
+  public reloadCommands(identifier:String):Number
+  public getArgs(msg:Discord.Message):Array<String>
+  public findUser(msg:Discord.Message):Discord.User
 }
 
+export class Command{
+  constructor(obj)
+  
+  public run():void
+  public areYou(cmd:String):Boolean
+}
+  
 
 }
